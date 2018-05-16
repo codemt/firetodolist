accf6e8e0d3958d07e646c08c3f1b4a81319299d
Mithilesh Tarkar <mithilesh.tarkar@ves.ac.in>
2018-05-16 15:03:49 +0530

    Initial commit

---
+++ src/config/db_config.js
@@ -0,0 +1,9 @@
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyC9qo09Q6gZUWMwDneMgv1oUmoBy_EBoi4",
+    authDomain: "firetodolist-7d367.firebaseapp.com",
+    databaseURL: "https://firetodolist-7d367.firebaseio.com",
+    projectId: "firetodolist-7d367",
+    storageBucket: "",
+    messagingSenderId: "465409271201"
+  };
+  
